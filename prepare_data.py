0742a10141e4a275ab5980c2efb308d690455de8
Anders Bogsnes <andersbognes@gmail.com>
2024-08-07 23:41:48 +0200

    Adding Dremio sources via prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -2,6 +2,7 @@ import pathlib
 import shutil
 import subprocess
 
+import httpx
 import minio
 from rich.console import Console
 from rich.progress import Progress, MofNCompleteColumn, BarColumn, TextColumn, TimeRemainingColumn
@@ -47,6 +48,51 @@ def upload_csvs(client: minio.Minio, csv_files: list[pathlib.Path]):
 
             shutil.copy(file, NOTEBOOK_DATA_DIR)
             progress.advance(upload_task)
+
+
+def create_dremio_sources(client: httpx.Client):
+    token = client.post("/apiv2/login", json={"userName": "dremio", "password": "dremio123"}).json()["token"]
+    client.headers.update({"Authorization": f"_dremio{token}"})
+
+    # Create Nessie source
+    payload = {
+        "entityType": "source",
+        "name": "Nessie",
+        "type": "NESSIE",
+        "config": {
+            "nessieEndpoint": "http://nessie:19120/api/v2",
+            "nessieAuthType": "NONE",
+            "awsAccessKey": "minio",
+            "awsAccessSecret": "minio1234",
+            "awsRootPath": "/warehouse",
+            "propertyList": [{
+                "fs.s3a.endpoint": "minio:9000",
+                "fs.s3a.path.style.access": "true",
+                "dremio.s3.compat": "true"
+            }]
+        }
+    }
+    resp = client.post("/api/v3/catalog", json=payload)
+    print(resp.json())
+
+    payload = {
+        "entityType": "source",
+        "name": "Extract",
+        "type": "S3",
+        "config": {
+            "accessKey": "minio",
+            "accessSecret": "minio1234",
+            "awsRootPath": "/datalake",
+            "secure": "false",
+            "compatibilityMode": "true",
+            "propertyList": [{
+                "fs.s3a.endpoint": "minio:9000",
+                "fs.s3a.path.style.access": "true",
+            }]
+        }
+    }
+    resp = client.post("/api/v3/catalog", json=payload)
+    print(resp.json())
 
 
 if __name__ == '__main__':
